f449d7264af690125d3ff891261abdd44c2e2e72
AlexejMatushkin <matushkin.leha2018@gmail.com>
2024-12-10 23:19:31 +0500

    Правка

--- api_yamdb/reviews/management/commands/import_data.py
+++ api_yamdb/reviews/management/commands/import_data.py
@@ -1,121 +1,108 @@
+import logging
 from csv import DictReader
 
 from django.core.management.base import BaseCommand
 
-from api_yamdb.settings import BASE_DIR
 from reviews.models import Category, Comments, Genre, GenreTitle, Review, Title
 from users.models import CustomUser
+from api.constants import CSV_DIR
 
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'Импорт данных из csv файлов'
 
-    def ImportUser(self):
-        if CustomUser.objects.exists():
-            print('Данные для User уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/users.csv',
-                    encoding='utf8')):
-                CustomUser.objects.create(
-                    id=row['id'],
-                    username=row['username'],
-                    email=row['email'],
-                    role=row['role'],
-                    bio=row['bio'],
-                    first_name=row['first_name'],
-                    last_name=row['last_name'],)
-            print('Данные для User загружены')
-
-    def ImportGenre(self):
-        if Genre.objects.exists():
-            print('Данные для Genre уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/genre.csv',
-                    encoding='utf8')):
-                Genre.objects.create(
-                    id=row['id'],
-                    name=row['name'],
-                    slug=row['slug'],)
-            print('Данные для genre загружены')
-
-    def ImportCategory(self):
-        if Category.objects.exists():
-            print('Данные для Category уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/category.csv',
-                    encoding='utf8')):
-                Category.objects.create(
-                    id=row['id'],
-                    name=row['name'],
-                    slug=row['slug'],)
-            print('Данные для Category загружены')
-
-    def ImportTitle(self):
-        if Title.objects.exists():
-            print('Данные для Title уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/titles.csv',
-                    encoding='utf8')):
-                Title.objects.create(
-                    id=row['id'],
-                    name=row['name'],
-                    year=row['year'],
-                    category=Category.objects.get(id=row['category']),)
-            print('Данные для Title загружены')
-
-    def ImportGenreTitle(self):
-        if GenreTitle.objects.exists():
-            print('Данные для GenreTitle уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/genre_title.csv',
-                    encoding='utf8')):
-                GenreTitle.objects.create(
-                    id=row['id'],
-                    title_id=row['title_id'],
-                    genre_id=row['genre_id'],)
-            print('Данные для GenreTitle загружены')
-
-    def ImportReview(self):
-        if Review.objects.exists():
-            print('Данные для Review уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/review.csv',
-                    encoding='utf8')):
-                Review.objects.create(
-                    id=row['id'],
-                    title_id=row['title_id'],
-                    text=row['text'],
-                    author=CustomUser.objects.get(id=row['author']),
-                    score=row['score'],
-                    pub_date=row['pub_date'],)
-            print('Данные для Review загружены')
-
-    def ImportComments(self):
-        if Comments.objects.exists():
-            print('Данные для Comments уже загружены')
-        else:
-            for row in DictReader(open(
-                    BASE_DIR / 'static/data/comments.csv',
-                    encoding='utf8')):
-                Comments.objects.create(
-                    id=row['id'],
-                    review_id=row['review_id'],
-                    text=row['text'],
-                    author=CustomUser.objects.get(id=row['author']),
-                    pub_date=row['pub_date'],)
-            print('Данные для Comments загружены')
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--force',
+            action='store_true',
+            help='Принудительно импортировать данные, даже если они уже существуют.',
+        )
 
     def handle(self, *args, **kwargs):
-        self.ImportUser()
-        self.ImportGenre()
-        self.ImportCategory()
-        self.ImportTitle()
-        self.ImportGenreTitle()
-        self.ImportReview()
-        self.ImportComments()
+        force = kwargs['force']
+        data_imports = [
+            (CustomUser, 'users.csv', self.create_user, 'Данные для User'),
+            (Genre, 'genre.csv', self.create_genre, 'Данные для Genre'),
+            (Category, 'category.csv', self.create_category, 'Данные для Category'),
+            (Title, 'titles.csv', self.create_title, 'Данные для Title'),
+            (GenreTitle, 'genre_title.csv', self.create_genre_title, 'Данные для GenreTitle'),
+            (Review, 'review.csv', self.create_review, 'Данные для Review'),
+            (Comments, 'comments.csv', self.create_comment, 'Данные для Comments'),
+        ]
+
+        for model, filename, create_method, data_label in data_imports:
+            self.import_data(model, filename, create_method, data_label, force)
+
+    def import_data(self, model, filename, create_method, data_label, force):
+        """Импорт данных из csv файла."""
+        if not force and model.objects.exists():
+            logger.info(f'{data_label} уже загружены. Используйте --force для повторного импорта.')
+            return
+
+        try:
+            with open(CSV_DIR / filename, encoding='utf8') as csvfile:
+                for row in DictReader(csvfile):
+                    create_method(row)
+            logger.info(f'{data_label} загружены.')
+        except Exception as e:
+            logger.error(f'Ошибка при импорте данных из {filename}: {e}')
+
+    def create_user(self, row):
+        CustomUser.objects.create(
+            id=row['id'],
+            username=row['username'],
+            email=row['email'],
+            role=row['role'],
+            bio=row['bio'],
+            first_name=row['first_name'],
+            last_name=row['last_name'],
+        )
+
+    def create_genre(self, row):
+        Genre.objects.create(
+            id=row['id'],
+            name=row['name'],
+            slug=row['slug'],
+        )
+
+    def create_category(self, row):
+        Category.objects.create(
+            id=row['id'],
+            name=row['name'],
+            slug=row['slug'],
+        )
+
+    def create_title(self, row):
+        Title.objects.create(
+            id=row['id'],
+            name=row['name'],
+            year=row['year'],
+            category=Category.objects.get(id=row['category']),
+        )
+
+    def create_genre_title(self, row):
+        GenreTitle.objects.create(
+            id=row['id'],
+            title_id=row['title_id'],
+            genre_id=row['genre_id'],
+        )
+
+    def create_review(self, row):
+        Review.objects.create(
+            id=row['id'],
+            title_id=row['title_id'],
+            text=row['text'],
+            author=CustomUser.objects.get(id=row['author']),
+            score=row['score'],
+            pub_date=row['pub_date'],
+        )
+
+    def create_comment(self, row):
+        Comments.objects.create(
+            id=row['id'],
+            review_id=row['review_id'],
+            text=row['text'],
+            author=CustomUser.objects.get(id=row['author']),
+            pub_date=row['pub_date'],
+        )
