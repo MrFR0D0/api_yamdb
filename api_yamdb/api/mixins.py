af2b1ab9a08c743832fc29c9db2a1159537fb141
AlexejMatushkin <matushkin.leha2018@gmail.com>
2024-12-12 19:07:49 +0500

    Правка

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -1,6 +1,16 @@
 from rest_framework.mixins import (CreateModelMixin, DestroyModelMixin,
-                                   ListModelMixin)
+                                   ListModelMixin, UpdateModelMixin)
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class UpdateOnlyPATCHMixin(UpdateModelMixin):
+    def update(self, request, *args, **kwargs):
+        """Отключает метод PUT, поддерживается только PATCH"""
+        if request.method == 'PUT':
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().update(request, *args, **kwargs)
 
 
 class ModelMixinSet(CreateModelMixin, ListModelMixin,
